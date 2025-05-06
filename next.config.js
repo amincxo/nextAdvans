385363db8014b72d080ffac71ed222bb958be273
unknown <amincxo@gmail.com>
2025-05-07 01:34:22 +0330

    tempetery redirect and permentent redirect

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: true, 
+  redirects: async () => {
+    return [
+        {source: "/react" , destination: "/next", permanent: false },
+        {source: "/about" , destination: "/", permanent: true }
+
+    ];
+  }
 }
 
 module.exports = nextConfig
