b2ee7f3e6d0e85c6385b9c12801fe5c2ac8aa79c
unknown <amincxo@gmail.com>
2025-05-07 01:59:25 +0330

    lang to farsi

--- pages/_document.js
+++ pages/_document.js
@@ -2,12 +2,12 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="fa" dir='rtl' >
       <Head >
             <meta name='author' content='amin borvayeh'  />
       </Head>
       <body>
-        <h1>amin</h1>
+        <h1>امین</h1>
         <Main />
         <NextScript />
       </body>
