71cd11a99eef7998b07dfa1eabb858224681ccee
unknown <amincxo@gmail.com>
2025-05-07 00:34:48 +0330

    image and responsive

--- pages/nature.js
+++ pages/nature.js
@@ -7,7 +7,7 @@ function Index() {
         <Image src='/images/1.jpeg' width={1000} height={700} alt='asdf' />
         <Image src='/images/2.jpeg' width={1000} height={700} alt='asdff' />
         <Image src='/images/3.jpeg' width={1000} height={700} alt='asdfsf' />
-        <Image src='/images/4.jpeg' width={1000} height={700} alt='asddfff' />
+        <Image src='/images/4.jpeg' width={700} height={700} alt='asddfff' />
     </div>
   )
 }
