40a5925116a1f28fffc01da9924f22c81aba304e
unknown <amincxo@gmail.com>
2025-05-07 01:55:07 +0330

    document in next

--- pages/_document.js
+++ pages/_document.js
@@ -3,8 +3,11 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head >
+            <meta name='author' content='amin borvayeh'  />
+      </Head>
       <body>
+        <h1>amin</h1>
         <Main />
         <NextScript />
       </body>
